4f12ed3720097b2b04d11e735090cd45a030d037
ndp69845 <ndp69845@gmail.com>
2020-03-04 23:46:01 +0000

    Just experimenting

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -3,6 +3,8 @@ package com.example.restservice;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +17,10 @@ public class GreetingController {
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-	}
+    }
+    
+    @PostMapping("/hello")
+    public Greeting hello(@RequestBody Hello hello) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, hello.getName()));
+    }
 }
